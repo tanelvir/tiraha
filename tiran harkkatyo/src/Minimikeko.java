88bc8e9454ab890d33b47ffa4d321a107c116048
Taneli Virkkala <Taneli Virkkala>
2012-08-06 23:08:02 +0300

    dokumentti päivitetty

--- tiran harkkatyo/src/Minimikeko.java
+++ tiran harkkatyo/src/Minimikeko.java
@@ -1,88 +1,115 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author tanelvir
- */
 
 import java.util.*;
 
+/*
+ * Minimikeko on tietorakenne, jossa solmut ovat aina suuruusjärjestyksessä pienimmästä suurempaan.
+ * Lisäksi siitä etsiminen on nopeaa (log n) sillä kaikilla paitsi pääsolmulla on vanhempi solmu.
+ * Lisäksi kaikilla paitsi viimeisillä solmuilla on lapsia.
+ */
 public class Minimikeko {
-    
+
     List<Solmu> h = new ArrayList<Solmu>();
 
-  public Minimikeko() {
-  }
-
-  public Minimikeko(Solmu[] keys) {
-    for (Solmu key : keys) {
-      h.add(key);
+    /**
+     * Luodaan keko tekemällä heapifyita.
+     *
+     * @param taulukko
+     */
+    public Minimikeko(Solmu[] taulukko) {
+        for (Solmu key : taulukko) {
+            h.add(key);
+        }
+        for (int k = h.size() / 2 - 1; k >= 0; k--) {
+            heapify(k, h.get(k));
+        }
     }
-    for (int k = h.size() / 2 - 1; k >= 0; k--) {
-      percolateDown(k, h.get(k));
+
+    /**
+     *
+     * Lisätään solmu kekoon katsomalla sille oikea sijainti keon
+     * järjestyksessä.
+     *
+     * @param solmu
+     */
+    public void lisaa(Solmu solmu) {
+        h.add(null);
+        int k = h.size() - 1;
+        while (k > 0) {
+            int vanhempi = (k - 1) / 2;
+            Solmu p = h.get(vanhempi);
+            if (solmu.compareTo(p) >= 0) {
+                break;
+            }
+            h.set(k, p);
+            k = vanhempi;
+        }
+        h.set(k, solmu);
     }
-  }
 
-  public void add(Solmu node) {
-    h.add(null);
-    int k = h.size() - 1;
-    while (k > 0) {
-      int parent = (k - 1) / 2;
-      Solmu p = h.get(parent);
-      if (node.compareTo(p) >= 0) {
-        break;
-      }
-      h.set(k, p);
-      k = parent;
+    /**
+     * Poistetaan solmu ja pidetään oikea järjestys tekemällä heapify
+     * viimeiselle solmulle.
+     *
+     * @return
+     */
+    public Solmu poista() {
+        Solmu poistettuSolmu = h.get(0);
+        Solmu viimeinenSolmu = h.remove(h.size() - 1);
+        heapify(0, viimeinenSolmu);
+        return poistettuSolmu;
     }
-    h.set(k, node);
-  }
 
-  public Solmu remove() {
-    Solmu removedNode = h.get(0);
-    Solmu lastNode = h.remove(h.size() - 1);
-    percolateDown(0, lastNode);
-    return removedNode;
-  }
-
-  public Solmu min() {
-    return h.get(0);
-  }
-
-  public boolean isEmpty() {
-    return h.isEmpty();
-  }
-
-  void percolateDown(int k, Solmu node) {
-    if (h.isEmpty()) {
-      return;
+    /**
+     * Palauttaa pienimmän solmun
+     *
+     * @return
+     */
+    public Solmu pienin() {
+        return h.get(0);
     }
-    while (k < h.size() / 2) {
-      int child = 2 * k + 1;
-      if (child < h.size() - 1 && h.get(child).compareTo(h.get(child + 1)) > 0) {
-        child++;
-      }
-      if (node.compareTo(h.get(child)) <= 0) {
-        break;
-      }
-      h.set(k, h.get(child));
-      k = child;
-    }
-    h.set(k, node);
-  }
 
-  // Usage example
-  public static void main(String[] args) {
-    Solmu[] taulu = {new Solmu(1), new Solmu(2), new Solmu(3), new Solmu(4)};
-    Minimikeko heap;
-    heap = new Minimikeko(taulu);
-    // print keys in sorted order
-    while (!heap.isEmpty()) {
-      System.out.println(heap.remove());
+    /**
+     * Palauttaa true jos keko on tyhjä, muuten false.
+     *
+     * @return
+     */
+    public boolean onkoTyhja() {
+        return h.isEmpty();
     }
-  }
-    
+
+    /**
+     * Katsotaan onko lapsi pienempi kuin vanhempansa ja onko lapsen viereinen
+     * solmu suurempi kuin se itse.
+     *
+     * @param k
+     * @param solmu
+     */
+    void heapify(int k, Solmu solmu) {
+        if (h.isEmpty()) {
+            return;
+        }
+        while (k < h.size() / 2) {
+            int lapsi = 2 * k + 1;
+            if (lapsi < h.size() - 1 && h.get(lapsi).compareTo(h.get(lapsi + 1)) > 0) {
+                lapsi++;
+            }
+            if (solmu.compareTo(h.get(lapsi)) <= 0) {
+                break;
+            }
+            h.set(k, h.get(lapsi));
+            k = lapsi;
+        }
+        h.set(k, solmu);
+    }
+
+    // Testausta
+    public static void main(String[] args) {
+        Solmu[] taulu = {new Solmu(2), new Solmu(4), new Solmu(1), new Solmu(3)};
+        Minimikeko heap;
+        heap = new Minimikeko(taulu);
+        // tulostetaan solmut järjestyksessä
+        while (!heap.onkoTyhja()) {
+            System.out.println(heap.poista());
+        }
+    }
 }
